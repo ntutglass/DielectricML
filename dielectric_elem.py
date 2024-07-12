0d8963150580652e043cd2a19bf9a1fcdc88a987
TimYTShih <65747526+TimYTShih@users.noreply.github.com>
2024-07-12 21:51:04 +0800

    Create dielectric_elem.py

---
+++ dielectric_elem.py
@@ -0,0 +1,20 @@
+#Column 1-14 correspond to SiO2, B2O3, Li2O, Na2O, K2O, MgO, CaO, SrO, BaO, ZnO, TiO2, Al2O3, Temp, and log f
+import numpy as np
+import pickle
+from sklearn import preprocessing
+
+#load and standardize data  
+cond = open("condition", "r")
+X = np.loadtxt(cond, usecols=range(39))
+X_std = preprocessing.scale(X)
+
+#load trained model
+pkl_filename = "pickle_model.pkl"
+with open(pkl_filename, 'rb') as file:
+    pickle_model = pickle.load(file)
+
+#test random sample
+Y_predictions = pickle_model.predict(X_std)
+
+#output result
+np.savetxt('output_predictions', Y_predictions) 
